c879db16f4038375123d2e13f7be55427b13f746
Tomazacre12 <shadow12122004@gmail.com>
2022-09-14 19:34:16 +0700

    them bai 5, 6

--- Lesson 5/app.js
+++
@@ -1,7 +0,0 @@
-function LoggerAndReturn(thing) {
-    return thing;
-}
-var message = LoggerAndReturn('Hello World');
-var message2 = LoggerAndReturn(2);
-console.log(message);
-console.log(message2);
--- Lesson 5/app.ts
+++
@@ -1,8 +0,0 @@
-function LoggerAndReturn<T>(thing: T) : T {
-    return thing
-}
-
-const message: string = LoggerAndReturn('Hello World')
-const message2: number = LoggerAndReturn(2)
-console.log(message)
-console.log(message2)
